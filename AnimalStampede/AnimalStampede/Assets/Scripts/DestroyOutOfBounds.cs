7a34fc4ab711781aa93877ab5b69ce50dba8ca3b
Joshua Tanczos <tanczosj@roverkids.org>
2025-04-15 13:31:45 -0400

    Completed Unit 2

--- AnimalStampede/AnimalStampede/Assets/Scripts/DestroyOutOfBounds.cs
+++ AnimalStampede/AnimalStampede/Assets/Scripts/DestroyOutOfBounds.cs
@@ -7,16 +7,20 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //If an object goes past the player delete that object
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
-        } else if (transform.position.z < lowerBound) { 
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
--- AnimalStampede/AnimalStampede/Assets/Scripts/SpawnManager.cs
+++ AnimalStampede/AnimalStampede/Assets/Scripts/SpawnManager.cs
@@ -5,22 +5,25 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
-                0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex],spawnPos,
-            animalPrefabs[animalIndex].transform.rotation);
-        }
+
+    }
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
+                    0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos,
+        animalPrefabs[animalIndex].transform.rotation);
     }
 }
