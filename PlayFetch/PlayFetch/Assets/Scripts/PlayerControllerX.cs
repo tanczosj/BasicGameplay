27a131de2d7dff9a825c881e089a1af865b2d685
Joshua Tanczos <tanczosj@roverkids.org>
2025-04-15 14:21:02 -0400

    Imported assets

--- PlayFetch/PlayFetch/Assets/Scripts/PlayerControllerX.cs
+++ PlayFetch/PlayFetch/Assets/Scripts/PlayerControllerX.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
+  
 {
+    private float leftLimit = -45;
+    private float bottomLimit = -5;
+    public float coolDownTimerDefault = 4f;
+    private float coolDownTimer = 0;
     public GameObject dogPrefab;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && coolDownTimer <= 0)
         {
+            coolDownTimer = coolDownTimerDefault;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+           
         }
+
+        coolDownTimer -= Time.deltaTime;
     }
 }
